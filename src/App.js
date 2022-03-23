4ddbdd29137f308ec2375bd928a74503eec4e99d
Shyam9538 <77747232+Shyam9538@users.noreply.github.com>
2022-03-23 15:55:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
